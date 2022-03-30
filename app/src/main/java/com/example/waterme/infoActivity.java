53599843ba47a9ed44be126e103c66bc1eb09968
pagn4 <agne.petke@gmail.com>
2022-03-30 21:02:23 +0300

    Went a different way and made navigation using icons on the toolbar.

--- app/src/main/java/com/example/waterme/infoActivity.java
+++ app/src/main/java/com/example/waterme/infoActivity.java
@@ -1,35 +1,31 @@
 package com.example.waterme;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-
-public class MainActivity extends AppCompatActivity {
+public class infoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_info);
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.nav_menu,menu);
         return true;
     }
+
 
     public boolean navigationMenuNextActivity(@NonNull MenuItem menuItem){
         switch (menuItem.getItemId()){
@@ -38,14 +34,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentOut);
                 this.overridePendingTransition(0, 0);
                 break;
-            case R.id.nav_info:
-                Intent intentSettings = new Intent(this, infoActivity.class);
+            case R.id.nav_home:
+                Intent intentSettings = new Intent(this, MainActivity.class);
                 startActivity(intentSettings);
                 this.overridePendingTransition(0, 0);
                 break;
         }
         return true;
     }
-
-
 }
--- app/src/main/java/com/example/waterme/outActivity.java
+++ app/src/main/java/com/example/waterme/outActivity.java
@@ -1,29 +1,25 @@
 package com.example.waterme;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-
-public class MainActivity extends AppCompatActivity {
+public class outActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_out);
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater=getMenuInflater();
@@ -33,10 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean navigationMenuNextActivity(@NonNull MenuItem menuItem){
         switch (menuItem.getItemId()){
-            case R.id.nav_out:
-                Intent intentOut = new Intent(this, outActivity.class);
+            case R.id.nav_home:
+                Intent intentOut = new Intent(this, MainActivity.class);
                 startActivity(intentOut);
                 this.overridePendingTransition(0, 0);
+
                 break;
             case R.id.nav_info:
                 Intent intentSettings = new Intent(this, infoActivity.class);
@@ -46,6 +43,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
-
-
 }
